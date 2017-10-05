9d1409f134ebb65b3f4325a040a5e2adeecc85ae
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-10-05 11:17:40 +0700

    :sparkle: Swimmers class added

--- VimGuard.py
+++ VimGuard.py
@@ -1,19 +1,19 @@
 import arcade
- 
+# This is bad. Please do not try.
+from models import *
+
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
- 
-class SpaceGameWindow(arcade.Window):
+SWIMMERS_AMOUNT = 4
+
+class GameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
-        arcade.set_background_color(arcade.color.BLACK)
- 
- 
+        arcade.set_background_color(arcade.color.WHITE)
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT, SWIMEMERS_AMOUNT)
     def on_draw(self):
         arcade.start_render()
- 
- 
+
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
---
+++ models.py
@@ -0,0 +1,17 @@
+class World:
+    def __init__(self, width, height, swimmers_amount):
+        self.width = width
+        self.height = height
+        self.swimmers = []
+        for i in range(swimmers_amount):
+            self.swimmers.append(Swimmer(300,300))
+    def update(self, delta):
+        self.ship.update(delta)
+
+class Swimmer:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.sinking = True
+    def update(self, delta):
+        pass
