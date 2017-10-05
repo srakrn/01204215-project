36f7d5f2d4bf319224fcd2a07f5faa5a54b71838
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-10-05 23:53:05 +0700

    :sparkle: Key events

--- VimGuard.py
+++ VimGuard.py
@@ -1,4 +1,5 @@
 import arcade
+import arcade.key
 # This is bad. Please do not try.
 from models import *
 
@@ -6,31 +7,19 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SWIMMERS_AMOUNT = 4
 
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
-        super().__init__(*args, **kwargs)
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
-
 class GameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.WHITE)
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT, SWIMMERS_AMOUNT)
-        self.swimmers_sprite = []
-        for i in range(SWIMMERS_AMOUNT):
-            self.swimmers_sprite.append(ModelSprite('imgs/swimmer-1.png', model=self.world.swimmers[i]))
     def on_draw(self):
         arcade.start_render()
-        for swimmer_sprite in self.swimmers_sprite:
-            swimmer_sprite.draw()
+        for swimmer in self.world.swimmers:
+            swimmer.draw()
     def update(self, delta):
         self.world.update(delta)
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 if __name__ == '__main__':
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
--- models.py
+++ models.py
@@ -1,34 +1,83 @@
+import arcade, arcade.key
+import random
+
+class SpriteTextures:
+    swimmer_normal = arcade.load_texture("imgs/swimmer-1.png", scale=1)
+    swimmer_fallen = arcade.load_texture("imgs/swimmer-1-fallen.png", scale=1)
+
 class World:
     def __init__(self, width, height, swimmers_amount):
         self.width = width
         self.height = height
         self.swimmers = []
+        self.time = 0
         for i in range(swimmers_amount):
             self.swimmers.append(Swimmer(50+(width-150)/4*i, 300))
+        self.swimmers[0].command = arcade.key.A
+        self.swimmers[1].command = arcade.key.S
+        self.swimmers[2].command = arcade.key.D
+        self.swimmers[3].command = arcade.key.F
     def update(self, delta):
+        self.time += delta
+        self.randomized = random.uniform(0,1)*2
+        if Swimmer.alive_swimmers > 0:
+            if self.randomized < self.time:
+                self.fallen_position = random.randint(0,3)
+                while(not self.swimmers[self.fallen_position].alive):
+                    self.fallen_position = random.randint(0,3)
+                self.swimmers[self.fallen_position].fallen()
+                self.time = 0
         for swimmer in self.swimmers:
             swimmer.update(delta)
+    def on_key_press(self, key, key_modifiers):
+        print("Key pressed: {}".format(key))
+        for swimmer in self.swimmers:
+            swimmer.on_key_press(key, key_modifiers)
 
 class Swimmer:
+    alive_swimmers = 0
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.alive = True
         self.sinking = False
         self.unsinking = False
+        self.text = ''
+        self.command = ''
+        self.sprite = arcade.Sprite()
+        self.sprite.texture = SpriteTextures.swimmer_normal
+        self.sprite.center_x = self.x
+        self.sprite.center_y = self.y
+        Swimmer.alive_swimmers += 1
+    def fallen(self):
+        self.sprite.texture = SpriteTextures.swimmer_fallen
+        self.sinking = True
     def sink(self):
         if self.sinking:
-            self.y -= 10
-        if self.y < 0:
+            self.y -= 2
+        if self.sprite.top < -20:
             self.alive = False
             self.sinking = False
             self.unsinking = False
+            Swimmer.alive_swimmers -= 1
     def unsink(self):
         if self.unsinking:
-            self.y += 10
-            if self.y>300:
-                self.unsinking = False
-                self.y = 300
+            self.y += 3
+        if self.y>300:
+            print("Unsinking")
+            self.unsinking = False
+            self.y = 300
+            self.sprite.texture = SpriteTextures.swimmer_normal
     def update(self, delta):
-        self.sink()
-        self.unsink()
+        if self.alive:
+            self.sink()
+            self.unsink()
+            self.sprite.center_x = self.x
+            self.sprite.center_y = self.y
+            self.sprite.update()
+    def draw(self):
+        self.sprite.draw()
+    def on_key_press(self, key, key_modifiers):
+        print("on_key_press; key = {} command = {}".format(key, self.command))
+        if key == self.command:
+            self.unsinking = True
