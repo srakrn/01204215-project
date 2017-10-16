b63254c2caacc4cc50844328a4e0b07771e3a537
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-10-16 22:22:50 +0700

    :sparkle: Added lists of Vim keys

--- models.py
+++ models.py
@@ -1,6 +1,82 @@
 import arcade, arcade.key
 import random
 
+class Commands:
+    command = [
+        {
+            "description": "Exiting vim without saving"
+            "command": ":q!"
+        },
+        {
+            "description": "Find"
+            "command": "/"
+        },
+        {
+            "description": "To the next find result"
+            "command": "n"
+        },
+        {
+            "description": "Insert mode to append"
+            "command": "a"
+        },
+        {
+            "description": "Jump to the beginning of the next word"
+            "command": "w"
+        },
+        {
+            "description": "Jump to the beginning of the previous word"
+            "command": "b"
+        },
+        {
+            "description": "Jump to the end of the next word"
+            "command": "e"
+        },
+        {
+            "description": "Jump to the first non-blank character of the line"
+            "command": "^"
+        },
+        {
+            "description": "Jump to the end of the line"
+            "command": "$"
+        },
+        {
+            "description": "Jump to the start of the line"
+            "command": "0"
+        },
+        {
+            "description": "Enter visual mode"
+            "command": "v"
+        },
+        {
+            "description": "Cursor left"
+            "command": "h"
+        },
+        {
+            "description": "Cursor down"
+            "command": "j"
+        },
+        {
+            "description": "Cursor up"
+            "command": "k"
+        },
+        {
+            "description": "Cursor right"
+            "command": "l"
+        },
+        {
+            "description": "Insert mode to the new lower line"
+            "command": "o"
+        },
+        {
+            "description": "Replace a single character"
+            "command": "r"
+        },
+        {
+            "description": "Delete character and substitute text"
+            "command": "s"
+        }
+    ]
+
 class SpriteTextures:
     swimmer_normal = arcade.load_texture("imgs/swimmer-1.png", scale=1)
     swimmer_fallen = arcade.load_texture("imgs/swimmer-1-fallen.png", scale=1)
@@ -14,14 +90,6 @@ class World:
         self.time = 0
         for i in range(swimmers_amount):
             self.swimmers.append(Swimmer(50+(width-150)/4*i, 275))
-        self.swimmers[0].command = arcade.key.A
-        self.swimmers[1].command = arcade.key.S
-        self.swimmers[2].command = arcade.key.D
-        self.swimmers[3].command = arcade.key.F
-        self.swimmers[0].text = "A"
-        self.swimmers[1].text = "S"
-        self.swimmers[2].text = "D"
-        self.swimmers[3].text = "F"
     def update(self, delta):
         self.time += delta
         self.randomized = random.uniform(0,1)*60
