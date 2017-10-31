2084bd9e4e26d6a235251d63e7305c6cbb7129af
Sirakorn Lamyai <tanersirakorn@gmail.com>
2017-10-31 22:08:18 +0700

    :beers: Went drunk (by boardgames)

---
+++ vim_commands.py
@@ -0,0 +1,62 @@
+commands = [
+    {
+        "description": "Find",
+        "command": "/"
+    },
+    {
+        "description": "To the next find result",
+        "command": "n"
+    },
+    {
+        "description": "Insert mode to append",
+        "command": "a"
+    },
+    {
+        "description": "Jump to the beginning of the next word",
+        "command": "w"
+    },
+    {
+        "description": "Jump to the beginning of the previous word",
+        "command": "b"
+    },
+    {
+        "description": "Jump to the end of the next word",
+        "command": "e"
+    },
+    {
+        "description": "Jump to the start of the line",
+        "command": "0"
+    },
+    {
+        "description": "Enter visual mode",
+        "command": "v"
+    },
+    {
+        "description": "Cursor left",
+        "command": "h"
+    },
+    {
+        "description": "Cursor down",
+        "command": "j"
+    },
+    {
+        "description": "Cursor up",
+        "command": "k"
+    },
+    {
+        "description": "Cursor right",
+        "command": "l"
+    },
+    {
+        "description": "Insert mode to the new lower line",
+        "command": "o"
+    },
+    {
+        "description": "Replace a single character",
+        "command": "r"
+    },
+    {
+        "description": "Delete character and substitute text",
+        "command": "s"
+    }
+]
